e3b6583c59cf70221ab5501cfaa1880aae5b4a15
DvMinhGithub <minhdv.dev@gmail.com>
2024-09-14 11:41:59 +0700

    feat: Add manage header

--- src/components/Header/ManageHeader.jsx
+++ src/components/Header/ManageHeader.jsx
@@ -1,5 +1,66 @@
+import { useCallback, useEffect } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+
+import useUserStore from '@/store/userStore'
+import { removeToken } from '@/utils'
+
+import { UserOutlined } from '@ant-design/icons'
+import { Avatar, Dropdown } from 'antd'
+
 const ManageHeaderComponent = () => {
-  return <h2>Header</h2>
+  const { getProfile, user } = useUserStore()
+  const navigate = useNavigate()
+  const { name, avatar } = user || {}
+
+  const handleLogout = useCallback(() => {
+    removeToken('token')
+    navigate('/login')
+  }, [navigate])
+
+  const menuItems = [
+    {
+      key: 'profile',
+      label: (
+        <Link to="/store-info" className="flex gap-2">
+          Tài khoản của tôi
+        </Link>
+      )
+    },
+    {
+      key: 'logout',
+      label: (
+        <button onClick={handleLogout} className="flex gap-2 w-full cursor-pointer">
+          Đăng xuất
+        </button>
+      )
+    }
+  ]
+
+  useEffect(() => {
+    getProfile()
+  }, [getProfile])
+
+  return (
+    <div className="container flex justify-between py-3 bg-white border-b">
+      <Link to="/" className="text-2xl font-bold text-blue-600 mr-8">
+        TechStore
+      </Link>
+      <div className="flex items-center w-fit">
+        <Dropdown menu={{ items: menuItems }} placement="bottomLeft" arrow>
+          <div className="flex items-center gap-3 cursor-pointer">
+            <span className="text-gray-800 text-base font-medium px-2.5 py-0.5 capitalize max-w-40 truncate">
+              {name}
+            </span>
+            {avatar ? (
+              <img src={avatar} alt="avatar" className="w-10 h-10 rounded-full object-cover" />
+            ) : (
+              <Avatar icon={<UserOutlined />} />
+            )}
+          </div>
+        </Dropdown>
+      </div>
+    </div>
+  )
 }
 
 export default ManageHeaderComponent
